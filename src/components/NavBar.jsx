6e77e46c6646edd988db2a565ddbd6262214bdca
MateusProDev <mateusferreiraprodev@gmail.com>
2024-05-23 11:13:24 -0300

    add imgs do carrinho/sacola

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 // src/components/NavBar.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css'; // Certifique-se de importar o arquivo de estilos
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css"; // Certifique-se de importar o arquivo de estilos
 
 const NavBar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -11,16 +11,19 @@ const NavBar = () => {
   };
 
   return (
-    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+    <nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
       <div className="logo-container">
         <img src="./img/Logo.png" alt="Logo" className="logo" />
       </div>
-      <div className="menu-icon" onClick={handleMenuToggle}>
-        <span className={`menu-line ${isMenuOpen ? 'open' : ''}`}></span>
-        <span className={`menu-line ${isMenuOpen ? 'open' : ''}`}></span>
-        <span className={`menu-line ${isMenuOpen ? 'open' : ''}`}></span>
+      <div className="boxCar">
+        <img src="./img/ShoppingBag.png" alt="CAR" />
+        <div className="menu-icon" onClick={handleMenuToggle}>
+          <span className={`menu-line ${isMenuOpen ? "open" : ""}`}></span>
+          <span className={`menu-line ${isMenuOpen ? "open" : ""}`}></span>
+          <span className={`menu-line ${isMenuOpen ? "open" : ""}`}></span>
+        </div>
       </div>
-      <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+      <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -34,7 +37,7 @@ const NavBar = () => {
           <Link to="/avaliações">Avaliações</Link>
         </li>
       </ul>
-      <nav className={`social-links ${isMenuOpen ? 'open' : ''}`}>
+      <nav className={`social-links ${isMenuOpen ? "open" : ""}`}>
         <ul className="nav-links-mobile">
           <li>
             <Link to="/">Home</Link>
@@ -49,7 +52,7 @@ const NavBar = () => {
             <Link to="/avaliações">Avaliações</Link>
           </li>
         </ul>
-        <ul className='social-links-icon'>
+        <ul className="social-links-icon">
           <li>
             <a href="#link-rede-social-1">
               <img src="./img/insta.png" alt="Rede Social" />
