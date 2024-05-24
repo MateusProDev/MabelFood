3348784620638fc29657a0b5bcb6a8603d3862e0
MateusProDev <mateusferreiraprodev@gmail.com>
2024-05-24 12:14:38 -0300

    add msgm personalizada para o pedido via whatssap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = ({ cart, total }) => {
   const handleFinalizePurchase = () => {
     const message = cart.map(item => `${item.name} - R$${item.price.toFixed(2)}`).join("\n");
     const totalValue = Number(total).toFixed(2); // Corrigido para garantir que total seja um número
-    const whatsappMessage = `Desejo concluir meu pedido:\n${message}\n\nTotal: R$${totalValue} <br /> Digite seu nome: seu endereço: e escolha a forma de pagamento: Pix, Debito, Credito`;
+    const whatsappMessage = `Desejo concluir meu pedido:\n\n ${message}\n\nTotal: R$${totalValue}\n\nPreencha as informações para a intrega: \n\n Nome:\nEndereço:\n Forma de pagamento: \nPix, Debito, Credito`;
     const whatsappUrl = `https://api.whatsapp.com/send?phone=5585991470709&text=${encodeURIComponent(whatsappMessage)}`;
     window.open(whatsappUrl, "_blank");
   };
