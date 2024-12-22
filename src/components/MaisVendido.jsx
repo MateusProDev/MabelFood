98036068deb3ae2c2898f44fa8776aff73c347b4
mateusprodev <mateusferreiraprodev@gmail.com>
2024-12-22 10:25:06 -0300

    add detalhes

--- src/components/MaisVendido.jsx
+++ src/components/MaisVendido.jsx
@@ -7,14 +7,15 @@ const products = [
     id: 1,
     name: "Pizza de Frango C/Catupiry",
     content: "Entregamos para toda sua região",
-    description: "Massa fresca, Molho de tomate, Frango, Queijo, Orégano e Azeitona",
+    description: "Massa fresca, Molho de tomate,<br />Frango, Queijo, Orégano e Azeitona",
     price: 39.99,
     imgSrc: "/img/frangoca.jpg",
-  },
+  }, 
   {
     id: 2,
     name: "Burguer Mac",
     content: "Entregamos para a sua região",
+    description: "Pão Brioche, Carne artesanal, Queijo,<br />Prejunto, Ovos, Tomate e Alface",
     price: 29.99,
     imgSrc: "/img/burguer.jpg",
   },
@@ -22,6 +23,7 @@ const products = [
     id: 3,
     name: "Pizza Marguerita",
     content: "Entregamos para a sua região",
+    description: "Massa fresca, Molho de tomate, Queijo,<br />Tomate em rodelas, Orégano e Azeitona",
     price: 39.99,
     imgSrc: "/img/marg.jpg",
   },
@@ -29,7 +31,7 @@ const products = [
     id: 4,
     name: "Pizza de Carne do Sol",
     content: "Entregamos para toda sua região",
-    description: "Massa fresca, Molho de tomate, Carne do sol, Queijo, Orégano e Azeitona",
+    description: "Massa fresca, Molho de tomate, Carne do sol,<br />Queijo, Orégano e Azeitona",
     price: 39.99,
     imgSrc: "/img/carnedosol.jpg",
   },
@@ -57,7 +59,9 @@ const MaisVendido = ({ addToCart }) => {
                 <img src={product.imgSrc} alt={product.name} />
                 <br />
                 <strong>{product.content}</strong>
-              <p>{product.description}</p>
+                <div className="boxDesc">
+                <p dangerouslySetInnerHTML={{ __html: product.description }}></p>
+                </div>
                 <div className="box-value">
                   <span>R${product.price.toFixed(2)}</span>
                   <button
